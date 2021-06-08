96d7112d9bd806d624c57b249e0d9f2d49386eff
Jose Maria Jimenez <Jose.Jimenez@kpit.com>
2021-06-08 07:49:00 +0200

    Additional table

--- db/migrate/20210608054502_create_search_results.rb
+++ db/migrate/20210608054502_create_search_results.rb
@@ -8,5 +8,13 @@ class CreateSearchResults < ActiveRecord::Migration[6.1]
 
       t.timestamps
     end
+
+    create_table :jokes_search_results, id: false do |t|
+      t.string :joke_id
+      t.bigint :search_result_id
+    end
+
+    add_index :jokes_search_results, :joke_id
+    add_index :jokes_search_results, :search_result_id
   end
 end
